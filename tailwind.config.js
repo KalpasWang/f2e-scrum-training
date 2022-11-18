f91a90f5c687776ef46fdbbb686933120030b546
Kalpas Wang <wf6212@gmail.com>
2022-11-18 18:25:04 +0800

    setup font, color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['"Cubic 11"', 'sans-serif'],
+    },
+    colors: {
+      primary1: '#5137FF',
+      primary2: '#FF60FA',
+      primary3: '#FFCB2D',
+      assist1: '#2B2B2B',
+      assist2: '#FFF9F6',
+      disabled: '#BDBDBD',
+    },
+    container: {
+      center: true,
+      padding: '1.5rem',
+    },
     extend: {
       minHeight: {
         3.75: '3.75rem',
