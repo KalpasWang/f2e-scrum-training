33ad3fc5983885ca1633d6a93dae944afd35e863
Kalpas Wang <wf6212@gmail.com>
2022-12-03 13:46:51 +0800

    add PriorityDnDStage test temp1

---
+++ src/tests/PriorityDnDStage.test.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import { PriorityDnDStage } from '../components/Stages';
+
+describe('PriorityDnDStage', () => {
+  describe('當重新開始遊戲', () => {
+    it('DnD 重置回預設的樣子', () => {
+      // arrange
+      render(<PriorityDnDStage />);
+
+      // act
+    });
+  });
+});
