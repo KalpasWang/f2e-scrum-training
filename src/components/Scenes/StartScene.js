25ff90c97073c79aeb2fa7041c39685072f3a935
KalpasWang <wf6212@gmail.com>
2023-03-12 23:42:17 +0800

    style start scene

--- src/components/Scenes/StartScene.js
+++ src/components/Scenes/StartScene.js
@@ -1,16 +1,41 @@
 import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import family from '../../assets/family.svg';
 
 export const StartScene = () => {
+  const navigate = useNavigate();
+
   return (
     <motion.main
-      className="flex h-full w-full items-center justify-center"
-      initial={{ opacity: 0, y: '-100%' }}
-      animate={{ opacity: 1, y: 0 }}
+      className="h-screen cursor-pointer bg-primary3 p-16"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
       transition={{ duration: 0.3 }}
-      exit={{ opacity: 0, y: '100%' }}
+      exit={{ opacity: 0 }}
+      onClick={() => {
+        navigate('training');
+      }}
     >
-      <Link to="training">開始</Link>
+      <div className="relative h-full bg-assist1">
+        <div className="absolute -inset-x-2 bottom-[5vh]">
+          <img
+            src={family}
+            alt="角色圖片"
+            className="max-h-[80vh] max-w-full"
+          />
+        </div>
+        <div className="absolute top-14 right-12 rounded-2xl border-4 border-dashed border-primary3 p-5">
+          <h2 className="text-center text-4xl leading-tight text-assist2 lg:text-6xl">
+            F2E<span className="text-primary3">帶你前進</span>
+          </h2>
+          <h1 className="text-5xl leading-tight text-primary2 lg:text-7xl">
+            敏捷騎士團
+          </h1>
+          <p className="text-center text-sm text-primary3 lg:text-xl">
+            Design by 讀書狗狗, Code by Kalpas
+          </p>
+        </div>
+      </div>
     </motion.main>
   );
 };
