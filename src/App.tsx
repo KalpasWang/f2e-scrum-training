018bd816f146bcfe31b1051512556df64ab26129
KalpasWang <wf6212@gmail.com>
2023-04-17 23:36:51 +0800

    加入背景音樂

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import React, { useEffect } from 'react';
+import useSound from 'use-sound';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import { StartScene, TrainingScene } from './components/Scenes';
 import { MainLayout } from './components/layouts';
 import './index.css';
-import React from 'react';
+import bgMusic from './assets/bgmusic.mp3';
 
 const router = createBrowserRouter([
   {
@@ -21,6 +23,16 @@ const router = createBrowserRouter([
 ]);
 
 export default function App() {
+  const [play, { stop }] = useSound(bgMusic, { loop: true, volume: 0.5 });
+
+  useEffect(() => {
+    play();
+
+    return () => {
+      stop();
+    };
+  }, []);
+
   return (
     <AnimatePresence mode="wait">
       <RouterProvider router={router} />;
